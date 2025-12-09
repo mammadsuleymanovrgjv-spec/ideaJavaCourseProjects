26b7fdb04229c1d580041e07913aa882a85ad98f
memmed <mammad.suleymanov.rg.jv@idtech.edu.az>
2025-12-09 05:18:55 +0400

    Exercise 1 solved!

---
+++ src/inheritance/exercise1/Animal.java
@@ -0,0 +1,7 @@
+package inheritance.exercise1;
+
+public class Animal {
+    public void sound(){
+        System.out.println("Animal");
+    }
+}
---
+++ src/inheritance/exercise1/Main.java
@@ -0,0 +1,10 @@
+package inheritance.exercise1;
+
+public class Main {
+    public static void main(String[] args) {
+        Animal dog = new Dog();
+        dog.sound();
+        Animal cat = new Cat();
+        cat.sound();
+    }
+}
