3d0df89b302c30aeddfa0ca6dd2838c2e7655bb5
memmed <mammad.suleymanov.rg.jv@idtech.edu.az>
2026-01-04 13:31:42 +0400

    add classPerson

---
+++ src/practise3_6_3/classPerson/Main.java
@@ -0,0 +1,10 @@
+package practise3_6_3.classPerson;
+
+public class Main {
+    public static void main(String[] args) {
+        Person person1 = new Person("Jhon", 25);
+        Person person2 = new Person("Fred", 35);
+        System.out.println(person1.toString());
+        System.out.println(person2.toString());
+    }
+}
---
+++ src/practise3_6_3/classPerson/Person.java
@@ -0,0 +1,23 @@
+package practise3_6_3.classPerson;
+
+public class Person {
+    private String name;
+    private int age;
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+    @Override
+    public String toString() {
+        return "Person{" + "name=" + name + ", age=" + age + '}';
+    }
+}
+
